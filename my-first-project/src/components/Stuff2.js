30853c56a05b143b727c3e8f361de7209abd46f4
Tim <ryalin1286@gmail.com>
2021-03-03 12:02:41 +0000

    Completed challenge 2

--- my-first-project/src/components/Stuff2.js
+++ my-first-project/src/components/Stuff2.js
@@ -1,9 +1,11 @@
 import Length from "./day03components/Length";
+import PasswordStrength from "./day03components/PasswordStrength";
 
 const Stuff2 = () => {
     return ( 
         <>
             <Length />       
+            <PasswordStrength />       
         </>
     );
 };
---
+++ my-first-project/src/components/day03components/PasswordStrength.js
@@ -0,0 +1,54 @@
+import { Component } from "react";
+
+// If it's empty the background shouldn't have styling.
+// If it's less than 9 it should be red.
+// If it's less than 16 it should be orange.
+// If it's 16 or more then it should be green.
+
+class PasswordStrength extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            input: "Test",
+            backgroundColor: "",
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+
+    handleChange(e) {
+
+        this.setState({ input: e.currentTarget.value });
+
+        if (this.state.input.length > 16) {
+            this.setState({ backgroundColor: "green" })
+        } else if (this.state.input.length > 9) {
+            this.setState({ backgroundColor: "orange" })
+        } else if (this.state.input.length < 9) {
+            this.setState({ backgroundColor: "red" })
+        }
+        
+    }
+
+    render() {
+
+        return (
+            <>
+                <input
+                    type="password"
+                    onChange={this.handleChange}
+                    style={{ backgroundColor: this.state.backgroundColor }}
+                    value={this.state.input}>
+                </input>
+                <p>{this.state.input.length}</p>
+            </>
+        );
+    }
+}
+
+export default PasswordStrength;
